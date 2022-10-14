0ec9a76e61ac8007da9bf4e35a042499f1cb42e0
moaaz-bhnas <moaaz_bs@yahoo.com>
2022-10-14 13:43:51 +0200

    Used Open Sans Google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        OpenSans: ["Open Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
